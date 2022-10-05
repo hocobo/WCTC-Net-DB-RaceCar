89ae728a0b3f7fbf119031876699b3a839acdf3a
hocobo <79883046+hocobo@users.noreply.github.com>
2022-10-05 18:32:39 -0500

    Initial Commit

---
+++ RaceTrack/Cars/BeaterTruck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class BeaterTruck:RaceCar
+    {
+        public BeaterTruck()
+        {
+            Name = "Jake's Beater Truck";
+            TopSpeed = 100;
+        }
+        public override void StartEngine()
+        {
+            Console.WriteLine($"{Name} starts up with no issues!");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"{Name}'s ABS goes off every time he brakes!");
+        }
+    }
+}
---
+++ RaceTrack/Drivers/Jake.cs
@@ -0,0 +1,18 @@
+﻿using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class Jake:Driver
+    {
+        public Jake(RaceCar car) : base(car)
+        {
+            Name = "Jake";
+            SkillLevel = 9;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+    }
+}
